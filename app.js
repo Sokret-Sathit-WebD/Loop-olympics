e8fab442bfc27a97e62c66cfacffb77d6f7f1b2f
sokret sathit <sokretsathit13@gmail.com>
2022-07-20 11:37:36 -0500

    Added PRELIMINARIES round

---
+++ app.js
@@ -0,0 +1,29 @@
+//PRELIMINARIES*******************
+
+// Write a for loop that prints to the console the numbers 0 through 9
+
+// const array = [1,2,3,4,5,6,7,8,9,]
+// for(let i = 0; i < array.length; i++)
+//     console.log(array[i])
+
+
+
+// Write a for loop that prints to the console 9 through 0
+
+// const array = [1,2,3,4,5,6,7,8,9,]
+// for(let i = array.length -1 ; i >= 0; i--) {
+//     console.log(array[i])
+// }
+
+
+//_____Write a for loop that will push every other fruit to an array
+
+// const fruit = ["banana", "orange", "apple", "kiwi", "peach", "mango"]
+// for(var i = 0; i < fruit.length; i+=2) {
+//     console.log(fruit[i]);
+// }
+
+
+//BRONZE MEDAL***********
+
+//
